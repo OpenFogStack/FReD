5008c3480067f3f256e612b0633bbd61975b222d
Trever Max Schirmer <t.schirmer@tu-berlin.de>
2019-12-02 10:58:12 +0100

    Init GoLand Project and setup script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FReD Devtools
+## Setup
+Please run the `./setup-git.sh` **in the `fred-devtools` (=this) folder**. It will clone the other projects into `src/`.
+When working with GoLand please open this folder.
--- setup-git.sh
+++ setup-git.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# Execute this in the fred-devtools folder!
 cd ..
-mkdir FReD
-mv -rd fred-devtools FReD/
+echo "Moving folder fred-devtools to FReD"
+mv fred-devtools FReD
 cd FReD
 
-git clone https://gitlab.tubit.tu-berlin.de/mcc-fred/fred-example-client.git
-git clone https://gitlab.tubit.tu-berlin.de/mcc-fred/fred-nase.git
-git clone https://gitlab.tubit.tu-berlin.de/mcc-fred/fred-node.git
+mkdir src
+cd src
+echo "Cloning into fred-example, fred-nase and fred-node"
+git clone git@gitlab.tubit.tu-berlin.de:mcc-fred/fred-example-client.git
+git clone git@gitlab.tubit.tu-berlin.de:mcc-fred/fred-nase.git
+git clone git@gitlab.tubit.tu-berlin.de:mcc-fred/fred-node.git
+
+echo "You can now open this project (folder FReD) in GoLand"
