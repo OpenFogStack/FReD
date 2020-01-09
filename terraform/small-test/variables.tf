39ac52f1e6299fbc32e155ac71740062fdce8956
Tobias Pfandzelter <pfandzelter@campus.tu-berlin.de>
2020-01-09 13:34:55 +0100

    move terraform variables around

--- terraform/small-test/variables.tf
+++ terraform/small-test/variables.tf
@@ -11,3 +11,20 @@ variable "instance_count" {
   type = number
   default = 3
 }
+
+variable "aws_access_key" {
+  description = "The AWS access key."
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key."
+}
+
+variable "gitlab_repo_username" {
+  description = "The username for the GitLab registry."
+}
+
+variable "gitlab_repo_password" {
+  description = "The password for the GitLab registry."
+}
+
