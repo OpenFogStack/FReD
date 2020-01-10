3ceb18d7193986c96ea55d27b58ab5fdc8c9fcbf
Tobias Pfandzelter <pfandzelter@campus.tu-berlin.de>
2020-01-10 12:21:42 +0100

    add extended test client

--- tests/extended-test-client.sh
+++ tests/extended-test-client.sh
@@ -1,72 +1,102 @@
 #!/usr/bin/env bash
-HOST=localhost
-PORT=9001
+
+# terraform plan -var="instance_count=2" -var="identifier=tp" --var-file="secret.tfvars"
+
+IDENTIFIER=tp
+DEFAULT_WEB_PORT=80
+
+ID_1=0
+HOST_1=$ID_1.nodes.$IDENTIFIER.mcc-f.red
+PORT_1=DEFAULT_WEB_PORT
+
+ID_2=1
+HOST_2=$ID_2.nodes.$IDENTIFIER.mcc-f.red
+PORT_2=DEFAULT_WEB_PORT
+
 APIVERSION=v0
 KEYGROUP_NAME=testgroup
 ID=1
 
+
+# Create a Keygroup with Host 1
 printf "\n"
-printf "Creating a Keygroup...\n"
-printf "Calling http://%s:%s/%s/keygroup/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME
+printf "Create a Keygroup with Host 1\n"
+printf "Calling http://%s:%s/%s/keygroup/%s\n" $HOST_1 $PORT_1 $APIVERSION $KEYGROUP_NAME
 
 curl --request POST -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/$KEYGROUP_NAME \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/keygroup/$KEYGROUP_NAME \
      -i
 
+# Register Node 2 at Node 1
 printf "\n"
-printf "Creating a Data Item in Keygroup...\n"
-printf "Calling PUT http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME $ID
+printf "Register Node 2 at Node 1\n"
+printf "Calling http://%s:%s/%s/replica\n" $HOST_1 $PORT_1 $APIVERSION
+
+curl --request POST -sL \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/replica \
+     --data "{\"id\":\"$ID_2\",\"addr\":\"$HOST_2\",\"port\":\"$PORT_2}" \
+     -i
+
+# Add Node 2 as a Replica Node for the Keygroup at Node 1
+printf "\n"
+printf "Add Node 2 as a Replica Node for the Keygroup at Node 1\n"
+printf "Calling http://%s:%s/%s/keygroup/%s/replica/%s\n" $HOST_1 $PORT_1 $APIVERSION $KEYGROUP_NAME ID_2
+
+curl --request POST -sL \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/keygroup/$KEYGROUP_NAME/replica/$ID_2 \
+     -i
+
+# Write an item to Node 1
+printf "\n"
+printf "Write an item to Node 1\n"
+printf "Calling PUT http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST_1 $PORT_1 $APIVERSION $KEYGROUP_NAME $ID
 
 curl --request PUT -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
      --data '{"data":"hello other world!"}' \
      -i
 
+# Read the item at Node 1
 printf "\n"
-printf "Reading Data Item from Keygroup...\n"
-printf "Calling GET http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME $ID
+printf "Read this item at Node 1\n"
+printf "Calling GET http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST_1 $PORT_1 $APIVERSION $KEYGROUP_NAME $ID
 
 curl --request GET -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
      -i
 
+# Read this item at Node 2
 printf "\n"
-printf "Updating Data Item in Keygroup...\n"
-printf "Calling PUT http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME $ID
-
-curl --request PUT -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
-     --data '{"data":"hello other world!"}' \
-     -i
-
-printf "\n"
-printf "Reading Data Item from Keygroup...\n"
-printf "Calling GET http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME $ID
+printf "Read this item at Node 2\n"
+printf "Calling GET http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST_2 $PORT_2 $APIVERSION $KEYGROUP_NAME $ID
 
 curl --request GET -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
+     --url http://$HOST_2:$PORT_2/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
      -i
 
+# Delete the item at Node 2
 printf "\n"
-printf "Deleting Data Item from Keygroup...\n"
-printf "Calling DELETE http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME $ID
+printf "Delete the item at Node 1\n"
+printf "Calling DELETE http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST_2 $PORT_2 $APIVERSION $KEYGROUP_NAME $ID
 
 curl --request DELETE -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
+     --url http://$HOST_2:$PORT_2/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
      -i
 
+# Read the item at Node 1
 printf "\n"
-printf "Reading Data Item from Keygroup...\n"
-printf "Calling GET http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME $ID
+printf "Read this item at Node 1\n"
+printf "Calling GET http://%s:%s/%s/keygroup/%s/items/%s\n" $HOST_1 $PORT_1 $APIVERSION $KEYGROUP_NAME $ID
 
 curl --request GET -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/keygroup/"$KEYGROUP_NAME"/data/"$ID" \
      -i
 
+# Delete the Keygroup at Node 1
 printf "\n"
-printf "Deleting the Keygroup...\n"
-printf "Calling DELETE http://%s:%s/%s/keygroup/%s\n" $HOST $PORT $APIVERSION $KEYGROUP_NAME
+printf "Delete the Keygroup at Node 1\n"
+printf "Calling DELETE http://%s:%s/%s/keygroup/%s\n" $HOST_1 $PORT_1 $APIVERSION $KEYGROUP_NAME
 
 curl --request DELETE -sL \
-     --url http://$HOST:$PORT/$APIVERSION/keygroup/$KEYGROUP_NAME \
+     --url http://$HOST_1:$PORT_1/$APIVERSION/keygroup/$KEYGROUP_NAME \
      -i
