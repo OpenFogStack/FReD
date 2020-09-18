95fa84412afdd66905fd16cc13577e5f03fb1ff5
Trever Max Schirmer <t.schirmer@tu-berlin.de>
2020-09-18 13:54:01 +0200

    Add GRPC Client and Server and refactor 3NodeTest
    fix tester Dockerfile
    
    fix 3nodetest

--- pkg/externalconnection/client.pb.go
+++ pkg/externalconnection/client.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.13.0
 // source: client.proto
 
-package client
+package externalconnection
 
 import (
 	context "context"
--- tester.Dockerfile
+++ tester.Dockerfile
@@ -14,6 +14,7 @@ RUN go mod download
 
 COPY tests tests
 COPY ext ext
+COPY pkg pkg
 
 RUN go install ./tests/3NodeTest/cmd/main/
 
