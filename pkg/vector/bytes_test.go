6e1f842526202783a37e1c927874727e65fe5698
Tobias Pfandzelter <tp@mcc.tu-berlin.de>
2023-02-07 16:48:39 +0100

    minor improvement

--- pkg/vector/bytes_test.go
+++ pkg/vector/bytes_test.go
@@ -19,3 +19,20 @@ func TestBytes(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, v.Compare(c, vclock.Equal))
 }
+
+func TestFromBytes(t *testing.T) {
+	v := vclock.New()
+	v["A"] = 1
+	v["B"] = 40
+	v["C"] = 0
+	v["D"] = 10
+
+	b := Bytes(v)
+
+	b[1] = b[1] ^ 0x11
+
+	c, err := FromBytes(b)
+
+	assert.Error(t, err)
+	assert.False(t, v.Compare(c, vclock.Equal))
+}
